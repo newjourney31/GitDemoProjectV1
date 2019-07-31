37a8ef392b92e919e23f324ba7aeee0be380bb02
User <User@192.168.0.13>
2019-07-31 17:06:49 -0400

    Committing to new branch

---
+++ src/DemoClassBranch.java
@@ -0,0 +1,10 @@
+
+public class DemoClassBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is created for the testing a branch on git-- ");
+
+	}
+
+}
